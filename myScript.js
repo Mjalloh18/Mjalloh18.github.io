357119be364ef9a5ad4802f2a830493ba584c816
Mjalloh18 <jalloh_mohamed@outlook.com>
2019-10-22 16:03:58 +0100

    test 6

---
+++ myScript.js
@@ -0,0 +1,22 @@
+function goldenButton() {
+  var x = document.getElementsByClassName("paragraph1") ;
+  x[0].innerHTML = "This is a golden retreiver!" ;
+  var y = document.getElementsByClassName("golden") ;
+  y[0].innerHTML = "Golden Retreiver";
+}
+
+function bulldogButton() {
+  var x = document.getElementsByClassName("paragraph1");
+  x[0].innerHTML = "This is a bulldog!";
+  var y = document.getElementsByClassName("golden")
+  y[0].innerHTML = "BullDog"
+
+}
+
+function poodleButton() {
+  var x = document.getElementsByClassName("paragraph1");
+  x[0].innerHTML = "This is a Poodle!";
+  var y = document.getElementsByClassName("golden")
+  y[0].innerHTML = "Poodle"
+
+}
