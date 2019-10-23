3338d26bb733a0b02eb7cb148792c417eeca79a1
Mjalloh18 <jalloh_mohamed@outlook.com>
2019-10-23 12:32:48 +0100

    test 6

--- myScript.js
+++ myScript.js
@@ -3,20 +3,41 @@ function goldenButton() {
   x[0].innerHTML = "This is a golden retreiver!" ;
   var y = document.getElementsByClassName("golden") ;
   y[0].innerHTML = "Golden Retreiver";
+  $("#bulldogImg").fadeOut();
+  $("#poodleImg").fadeOut();
+  $("#goldenRetrevierImg").fadeIn();
+
+  $("#bulldogInfo").show();
+  $("#poodleInfo").show();
+  $("#goldenRetrevierInfo").hide()
 }
 
 function bulldogButton() {
   var x = document.getElementsByClassName("paragraph1");
   x[0].innerHTML = "This is a bulldog!";
-  var y = document.getElementsByClassName("golden")
-  y[0].innerHTML = "BullDog"
+  var y = document.getElementsByClassName("golden");
+  y[0].innerHTML = "BullDog" ;
+  $("#bulldogImg").fadeIn();
+  $("#poodleImg").fadeOut();
+  $("#goldenRetrevierImg").fadeOut();
+
+  $("#bulldogInfo").hide();
+  $("#poodleInfo").show();
+  $("#goldenRetrevierInfo").show()
 
 }
 
 function poodleButton() {
   var x = document.getElementsByClassName("paragraph1");
   x[0].innerHTML = "This is a Poodle!";
-  var y = document.getElementsByClassName("golden")
-  y[0].innerHTML = "Poodle"
+  var y = document.getElementsByClassName("golden");
+  y[0].innerHTML = "Poodle";
+  $("#bulldogImg").fadeOut();
+  $("#poodleImg").fadeIn();
+  $("#goldenRetrevierImg").fadeOut();
 
+  $("#bulldogInfo").show();
+  $("#poodleInfo").hide();
+  $("#goldenRetrevierInfo").show()
 }
+</script>
